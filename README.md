10de4ada79cab7344ff16f27fac2518eadfe8029
Raihan Ramadistra <ramadistra@gmail.com>
2019-04-21 20:09:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-compfest
+
+COMPFEST es-lint rules
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-compfest`:
+
+```
+$ npm install eslint-plugin-compfest --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-compfest` globally.
+
+## Usage
+
+Add `compfest` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "compfest"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "compfest/compfest-uppercase": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/compfest-uppercase.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview COMPFEST
+ * @author Raihan Ramadistra
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/compfest-uppercase"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("compfest-uppercase", rule, {
+
+    valid: [
+        "message = 'Welcome to COMPFEST!'",
+        "test()",
+        "null",
+        "1",
+        "1 + 1",
+        "false"
+    ],
+
+    invalid: [
+      {
+        code: "message = \"Welcome to Compfest!\";",
+        output: "message = \"Welcome to COMPFEST!\";",
+        errors: [{
+            message: "Replace Compfest with COMPFEST.",
+        }]
+      },
+      {
+        code: "message = \"Welcome to Compfest & compfest!\";",
+        output: "message = \"Welcome to COMPFEST & COMPFEST!\";",
+        errors: [{
+            message: "Replace Compfest, compfest with COMPFEST.",
+        }]
+      },
+      {
+        code: "message = { title: 'Compfest Grand Launching' };",
+        output: "message = { title: 'COMPFEST Grand Launching' };",
+        errors: [{
+            message: "Replace Compfest with COMPFEST.",
+        }]
+      },
+    ]
+});
